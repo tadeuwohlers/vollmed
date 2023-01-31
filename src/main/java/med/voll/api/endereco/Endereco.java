b29759ea4133edf30abc07bd2974162527195b7a
Tadeu Wohlers <tadeuwohlers@outlook.com>
2023-01-30 21:45:43 -0300

    first commit

---
+++ src/main/java/med/voll/api/endereco/Endereco.java
@@ -0,0 +1,33 @@
+package med.voll.api.endereco;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.Embedded;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import med.voll.api.medico.DadosEndereco;
+
+@Embeddable
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Endereco {
+    private String logradouro;
+    private String bairro;
+    private String cep;
+    private String numero;
+    private String complemento;
+    private String cidade;
+    private String uf;
+
+    public Endereco(DadosEndereco endereco) {
+        this.logradouro = endereco.logradouro();
+        this.bairro = endereco.bairro();
+        this.cep = endereco.cep();
+        this.uf = endereco.uf();
+        this.cidade = endereco.cidade();
+        this.numero = endereco.numero();
+        this.complemento = endereco.complemento();
+    }
+}
